434745f1bcbbd40c882fd612f32e18bcae4921e7
FernandoVaz <fndmvaz@gmail.com>
2021-03-16 11:19:04 -0300

    Teste git VSC

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+
+const ExpenseList = () => {
+    const expenses = [
+        { id: 12, name: 'shopping', cost: 40},
+        { id: 13, name: 'testing', cost: 100},
+        { id: 14, name: 'shopping', cost: 120},
+    ];
+
+    return (
+        <ul className='list-group'>
+            {expenses.map((expense) => (
+                <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
+            ))}
+        </ul>
+    );
+};
+
+export default ExpenseList
